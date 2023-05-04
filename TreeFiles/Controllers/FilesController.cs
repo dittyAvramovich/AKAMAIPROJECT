6fcb801e0d055e7502ba11f92c331c1aa68a0814
dittyAvramovich <67556022+dittyAvramovich@users.noreply.github.com>
2023-05-04 08:56:01 +0300

    Add files via upload

---
+++ TreeFiles/Controllers/FilesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System;
+
+
+namespace TreeFiles.Controllers
+{
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]")]
+
+        public IEnumerable<Directory> Get()
+        {
+            return GetAllData();
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{prefix}")]
+        public IEnumerable<Directory> Search([FromRoute]string prefix)
+        {
+            var data = GetAllData().ToList();
+            var dataFilter = new List<Directory>();
+            for (int i = 0; i < data.ToList().Count; i++)
+            {
+                data[i] = FilterByPrefix(prefix, data[i]);
+                if (!data[i].Directories.Any())
+                    data.RemoveAt(i);
+            }
+            return data;
+        }
+
+
+        private IEnumerable<Directory> GetAllData()
+        {
+            string text = System.IO.File.ReadAllText(@"./data.json");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var directories = JsonSerializer.Deserialize<List<Directory>>(text, options);
+            return directories;
+        }
+
+        private Directory FilterByPrefix(string prefix, Directory? directory)
+        {
+            directory.Files = directory.Files.ToList().Where(p => p.EndsWith(prefix)).ToList();
+            if (directory.Directories.Any())
+            {
+                for (int i = 0; i < directory.Directories.Count; i++)
+                {
+                    var result = FilterByPrefix(prefix, directory.Directories[i]);
+                    if (result.Files.Any())
+                        directory.Directories[i] = result;
+                    else directory.Directories.RemoveAt(i);
+                }
+            }
+            return directory;
+        }
+
+    }
+
+}
---
+++ TreeFiles/Directory.cs
@@ -0,0 +1,9 @@
+﻿namespace TreeFiles
+{
+    public class Directory
+    {
+        public string Name { get; set; }
+        public List<string> Files { get; set; }
+        public List<Directory> Directories { get; set; }
+    }
+}
